c906084e5bfd1456a9b878984168d820d8daa029
CahyaProject <56394451+CahyaProject@users.noreply.github.com>
2019-10-11 21:49:36 +0700

    Add files via upload

---
+++ SadapWhatsApp.sh
@@ -0,0 +1,60 @@
+#usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+figlet -f slant "Sadap Whatsapp" | lolcat
+echo $ku"BY : MR.$€V£N"
+echo $i"Loading..."
+sleep 5
+echo "Sadap Chat Wa Work 100%" | lolcat
+echo $cy"Masukkan Nomor Lu"
+read -p"====> " pil;
+echo $cy"Masukkan Nomor Target"
+read -p"====> " bro;
+echo $cy"Masukkan Nama Target $bro"
+read -p"====> " kei;
+
+echo $pur"Scanning..."
+sleep 6
+echo $i"Checking $bro"
+sleep 5
+echo $ku"Mengambil Chat Terbaru $bro..."
+sleep 5
+echo $me"Chat Terakhir $bro"
+echo $bi"1.+62 822-5834-6655"
+echo $bi"2.+62 813-2597-1378"
+echo $bi"3.+62 812-6017-0402"
+echo $i"Pilih Nomor"
+read -p"====> " nor;
+
+if [ $nor = 1 ]
+then
+echo $me"Mengambil Chat..."
+sleep 5
+echo $i"Adesta : Assalamualaikum $kei"
+echo $cy"$kei : Walaikumsalam,Ada Apa Yank?"
+echo $i"Adesta : Vc Yuk Yank Sembari Kita Curhat!"
+echo $cy"$kei : Ayok Mumpung Doi Ku Gak Online :)"
+fi
+
+if [ $nor = 2 ]
+then
+echo $me"Mengambil Chat..."
+sleep 5
+echo $i"Ilham : Save Back Ilham"
+echo $cy"$kei : Ok"
+fi
+
+if [ $nor = 3 ]
+then
+echo $i"Putra : P"
+echo $cy"$kei : Apa Si Nge P Mulu :("
+echo $i"Putra : Apa Si Gw Aja Ngecek Paketan Asu :v"
+fi
